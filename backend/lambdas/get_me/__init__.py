1961c2622966bd642ce1478ba71ec62fc494d2d6
junah201 <junah.dev@gmail.com>
2025-01-19 15:14:00 +0900

    [BE] feat: Add get_me lambda

---
+++ backend/lambdas/get_me/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+import boto3
+from pydantic import BaseModel
+
+from shared import authorizer, get_logger, middleware
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ.get("DYNAMODB_TABLE", "modurank-db"))
+
+logger = get_logger()
+
+
+class UserPublic(BaseModel):
+    user_id: str
+    email: str
+    nickname: str
+
+
+@middleware(logger=logger)
+@authorizer(logger=logger)
+def handler(event, _context):
+    user = event["requestContext"]["user"]
+
+    user_obj = UserPublic.model_validate(user)
+
+    return {
+        "statusCode": 200,
+        "body": user_obj.dict(),
+    }
